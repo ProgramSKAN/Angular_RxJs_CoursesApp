001bf258913cf727040b857ef98cf45f8c16a021
SandeepKandagatla <kandagatlas@gmail.com>
2020-05-08 15:20:09 +0530

    Custom-Events2::::Added delete operation
    it is not scalable , so use observable pattern

--- src/app/lessons-list/lessons-list.component.ts
+++ src/app/lessons-list/lessons-list.component.ts
@@ -1,7 +1,7 @@
 import { Lesson } from './../shared/model/lesson';
 import { globalEventBus, Observer, LESSONS_LIST_AVAILABLE, ADD_NEW_LESSON } from './../event-bus-experiments/event-bus';
 import { Component, OnInit } from '@angular/core';
-
+import * as _ from 'lodash';
 
 @Component({
   selector: 'lessons-list',
@@ -11,6 +11,7 @@ import { Component, OnInit } from '@angular/core';
 export class LessonsListComponent implements OnInit, Observer {
 
   lessons: Lesson[] = [];
+
   constructor() {
     //placed in the constructor, since we need to register observer before broadcasting>disadvantage
     console.log('lessons list component is registerd as observer..')
@@ -36,5 +37,8 @@ export class LessonsListComponent implements OnInit, Observer {
   toggleLessonViewed(lesson: Lesson) {
     console.log('toggling lesson ...');
     lesson.completed = !lesson.completed;
+  }
+  delete(deleted: Lesson) {
+    _.remove(this.lessons, lesson => lesson.id === deleted.id);
   }
 }
